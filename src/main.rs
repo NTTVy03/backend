ba2a14322041c50fbcb624ced9acb98d84c98baf
NTTVy03 <trucvy05062003@gmail.com>
2024-08-02 17:22:00 +0700

    example: shared mutable state

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
-use actix_web::{web, get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, App, HttpServer};
+use std::sync::Mutex;
 
 struct AppState {
-    app_name: String,
+    counter: Mutex<i32>,
 }
 
 #[get("/")]
-async fn hello(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    // HttpResponse::Ok().body("Hello {}!", app_name)
-    format!("Hello {}!", app_name)
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-#[get("/hey")]
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn count(data: web::Data<AppState>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Number {}!", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(
+        AppState{
+            counter: Mutex::new(0)
+        }
+    );
+
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(
-                AppState{
-                    app_name: String::from("ZyZy web")
-                }
-            ))
-            .service(hello)
-            .service(echo)
-            .service(manual_hello)
+            .app_data(counter.clone())
+            .service(count)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
